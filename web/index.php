b51d4ab84467a3c215b188ec2c8d6450e64dfeb9
kaerudisny <kaerudisny@gmail.com>
2016-04-27 16:43:54 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -45,7 +45,7 @@ $app->post('/callback', function (Request $request) use ($app) {
                         'id' => $from, 
                     ],
                     'message' => [
-                        'text' => sprintf('%sじゃない', $text), 
+                        'text' => sprintf('%sだよよ', $text), 
                     ],
                 ];
                 $client->request('POST', $path, ['json' => $json]);
